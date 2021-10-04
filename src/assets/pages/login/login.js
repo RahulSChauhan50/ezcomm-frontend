a9541ce30fce05c9e6b9fbe6cca2d7cd66d366e4
RahulSChauhan50 <rahulschauhan50@gmail.com>
2021-10-04 21:51:32 +0530

    installed bootstrap,react router, styled head body root of index.html, added login and error 404 page

---
+++ src/assets/pages/login/login.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./login.css";
+export class login extends Component {
+  render() {
+    return (
+      <div className='container'>
+        Login page
+        <div id="t">red</div>
+      </div>
+    );
+  }
+}
+
+export default login;
---
+++ src/assets/pages/notfoundpage/notfoundpage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class notfoundpage extends Component {
+  render() {
+    return <div>ERROR 404 page not found</div>;
+  }
+}
+
+export default notfoundpage;
