a43b806f6924b3c8a3c39ce15053ac4d9f633c74
RahulSChauhan50 <rahulschauhan50@gmail.com>
2021-10-10 14:14:42 +0530

    Some chages to login.js and sidebar.css

--- src/assets/pages/login/login.js
+++ src/assets/pages/login/login.js
@@ -1,37 +1,40 @@
 import React, { Component } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { Link } from "react-router-dom";
 import "./login.css";
 export class login extends Component {
   render() {
     return (
       <div className="container">
-     <div class="login-page">
-  <div class="form">
-    <form class="register-form">
-      <input type="text" placeholder="name"/>
-      <input type="password" placeholder="password"/>
-      <input type="text" placeholder="email address"/>
-      <button>create</button>
-      <p class="message">Already registered? <a href="#">Sign In</a></p>
-    </form>
-    <form class="login-form">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <select class="selectpicker"  data-live-search="true">
-      <option selected disabled>User Type</option>
-  <option data-tokens="Student" title="Uer Type">Student</option>
-  <option data-tokens="Faculty">Faculty</option>
-  <option data-tokens="HOD">HOD</option>
-  <option data-tokens="Principal">Principal</option>
+        <div className="login-page">
+          <div className="form">
+            <form className="register-form">
+              <input type="text" placeholder="name" />
+              <input type="password" placeholder="password" />
+              <input type="text" placeholder="email address" />
+              <button>create</button>
+              <p className="message">
+                Already registered? <a href="#">Sign In</a>
+              </p>
+            </form>
+            <form className="login-form">
+              <input type="text" placeholder="username" />
+              <input type="password" placeholder="password" />
+              <select className="selectpicker" data-live-search="true">
+                <option selected disabled>
+                  Login As
+                </option>
+                <option value="Student">Student</option>
+                <option value="Faculty">Faculty</option>
+                <option value="HOD">HOD</option>
+                <option value="Principal">Principal</option>
+              </select>
 
-</select>
-
-      <button>login</button>
-      <p class="message">Not registered? <a href="#">Create an account</a></p>
-    </form>
-  </div>
-</div> 
+              <button>login</button>
+              <p className="message">
+                Not registered? <a href="#">Create an account</a>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
     );
   }
