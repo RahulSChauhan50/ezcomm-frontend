31eb26482fe1b2d05c191dc6b62156433164254a
deepika-mahendra <deepikamahendra805@gmail.com>
2021-10-10 11:44:52 +0530

    login updated

--- src/assets/pages/login/login.js
+++ src/assets/pages/login/login.js
@@ -2,78 +2,38 @@ import React, { Component } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
 import "./login.css";
-
-
-
 export class login extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'coconut'};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('Your favorite flavor is: ' + this.state.value);
-    event.preventDefault();
-  }
   render() {
     return (
       <div className="container">
-          <div class="parent clearfix">
-    <div class="bg-illustration">
-      {/* <img src="https://img.collegedekhocdn.com/media/img/institute/logo/gec.jpeg" alt="logo"></img> */}
+     <div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="username"/>
+      <input type="password" placeholder="password"/>
+      <select class="selectpicker"  data-live-search="true">
+      <option selected disabled>User Type</option>
+  <option data-tokens="Student" title="Uer Type">Student</option>
+  <option data-tokens="Faculty">Faculty</option>
+  <option data-tokens="HOD">HOD</option>
+  <option data-tokens="Principal">Principal</option>
 
-      <div class="burger-btn">
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
+</select>
 
-    </div>
-    
-    <div class="login">
-      <div class="container">
-        <h1>Login</h1>
-    
-        <div class="login-form">
-          
-          <form action="">
-            
-            <input type="email" placeholder="E-mail Address"></input>
-            <input type="password" placeholder="Password"></input>
-            <label>
-          user type:
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">Student</option>
-            <option value="lime">faculty</option>
-            <option value="coconut">Principal</option>
-            
-          </select>
-        </label>
-
-            
-            {/* <div class="forget-pass">
-              <a href="#">Forgot Password ?</a>
-            </div> */}
-
-            <button type="submit">LOG-IN</button>
-
-          </form>
-        </div>
-    
-      </div>
-      </div>
+      <button>login</button>
+      <p class="message">Not registered? <a href="#">Create an account</a></p>
+    </form>
   </div>
+</div> 
       </div>
-      
     );
-    
   }
 }
 
