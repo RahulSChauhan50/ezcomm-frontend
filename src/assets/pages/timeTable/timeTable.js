caa4afc4c745afabaee986973e82a1334ffe61f6
RahulSChauhan50 <rahulschauhan50@gmail.com>
2021-12-31 01:30:49 +0530

    finished basic table layout

--- src/assets/pages/timeTable/timeTable.js
+++ src/assets/pages/timeTable/timeTable.js
@@ -51,51 +51,68 @@ class timeTable extends Component {
           <table className="table table-hover table-nowrap">
             <thead className="thead-light">
               <tr>
-                <th scope="col" className="topic">
+                <th scope="col" className="time">
                   Day
                 </th>
-                <th scope="col">Subject Name</th>
-                <th scope="col">Submitted By</th>
-                <th scope="col">Issue Date</th>
-                <th scope="col">Plagiarised check</th>
-                <th scope="col">View Submissions</th>
+                <th scope="col" className="time">
+                  <input
+                    type="time"
+                    required
+                    value={"09:05"}
+                    disabled
+                    onChange={(event) => console.log(event.target.value)}
+                  />
+                  <br /> to
+                  <br /> 09:05 AM
+                </th>
+                <th scope="col" className="time">
+                  09:05 AM
+                  <br /> to
+                  <br /> 09:05 AM
+                </th>
+                <th scope="col" className="time">
+                  09:05 AM
+                  <br /> to
+                  <br /> 09:05 AM
+                </th>
+                <th scope="col" className="time">
+                  09:05 AM
+                  <br /> to
+                  <br /> 09:05 AM
+                </th>
+                <th scope="col" className="time">
+                  09:05 AM
+                  <br /> to
+                  <br /> 09:05 AM
+                </th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="topic">
-                  <a href="#" className="font-semibold">
-                    title
-                  </a>
-
-                  <br />
-                </td>
-                <td>
-                  <span>subject</span>
-                </td>
-                <td>
-                  <span>Student</span>
-                </td>
-                <td>
-                  <span>date</span>
-                </td>
-                <td>
-                  <div className="plagiarismicon">sign</div>
-                </td>
-                <td>
-                  <Button className="status status-paid">View</Button>
-                </td>
-              </tr>
+              {this.state.days.map((val, ind) => (
+                <tr key={ind}>
+                  <td className="time">
+                    <span className="font-semibold">{val}</span>
+                  </td>
+                  <td className="time">
+                    <span>subject</span>
+                  </td>
+                  <td className="time">
+                    <span>Student</span>
+                  </td>
+                  <td className="time">
+                    <span>date</span>
+                  </td>
+                  <td className="time">
+                    <div className="plagiarismicon">sign</div>
+                  </td>
+                  <td className="time">
+                    <span>subject</span>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
-        <input
-          type="time"
-          required
-          value={"09:05"}
-          disabled
-          onChange={(event) => console.log(event.target.value)}
-        />
       </div>
     );
   }
